f5185ab4e63836e904220e2c8ecaf6d97e96cd15
totrebor <roberto@taglioretti.it>
2024-05-18 16:24:14 +0200

    project cleaning

--- sources/measures/ldbox.py
+++ sources/measures/ldbox.py
@@ -3,12 +3,15 @@
 
 class LDBox:
     
+    obbm = None
+    obbo = None
 
     def __init__(self, cloud):
         self._cloud = cloud
 
 
     def run(self):
-        obb = self._cloud.get_oriented_bounding_box()
-        obbr = self._cloud.get_oriented_bounding_box(robust=True)
-        return { 'obb': obb, 'obbr': obbr }
+        self.obbmin = self._cloud.get_minimal_oriented_bounding_box()
+        self.obbmax = self._cloud.get_oriented_bounding_box()
+
+        return { 'obbmin': self.obbmin, 'obbmax': self.obbmax }
